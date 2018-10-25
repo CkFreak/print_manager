a7c3b99ee9fc649841814821a660e6baec1f11fd
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-10-25 15:08:01 +0200

    Creates a server file to start the server

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import { MongoService } from "./services/mongo_service";
+import { App } from "./app";
+import { PORT } from "./config/constants";
+
+const start =  async () => {
+    MongoService.init().then((mongo) => {
+        const app = App(mongo);
+        app.listen(PORT);
+    });
+};
+
+start().then(() => {
+    console.log(`Server started! Listening on Port ${PORT}.`);
+});
