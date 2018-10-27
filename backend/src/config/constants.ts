a654a010e1b9982c490226852f80b490fb3f3924
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-10-27 15:22:43 +0200

    Adds a secret for the jwt login token

--- backend/src/config/constants.ts
+++ backend/src/config/constants.ts
@@ -2,3 +2,4 @@ export const PORT = 3001;
 export const MONGO_CONNECTION_STRING = "mongodb://localhost:27017/print_manager";
 
 export const CH_DRUCK_PW = process.env.CH_DRUCK_PW;
+export const LOGIN_SECRET = process.env.LOGIN_SECRET;
