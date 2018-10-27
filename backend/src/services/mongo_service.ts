2c198f7439249b4f156934d84dfc03b850906264
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-10-27 15:24:01 +0200

    Adds a method to register a user and a method to retrieve a user

--- backend/src/services/mongo_service.ts
+++ backend/src/services/mongo_service.ts
@@ -1,20 +1,34 @@
 import * as mongoose from "mongoose";
 import { MONGO_CONNECTION_STRING } from "../config/constants";
 import { MongoServiceT } from "../types/services/mongo_service";
+import { UserModel } from "../models/UserModel";
 
 export const MongoService = (() => {
 
-    const mongo: MongoServiceT = {
-
+    const registerUser = (name: string, password: string): Promise<any> => {
+        return UserModel.update({name: name},
+            {name: name, password: password},
+            {upsert: true})
+            .exec();
     };
-   const init =  () => {
-       return mongoose.connect(MONGO_CONNECTION_STRING, {useNewUrlParser: true}).then(() => {
-           console.log("Database connection established");
-           return mongo;
-       });
-   };
 
-   return {
-       init,
-   }
+    const getUserByName = (name: string): Promise<any> => {
+        return UserModel.findOne({name: name}).exec();
+    };
+
+    const mongo: MongoServiceT = {
+        registerUser,
+        getUserByName,
+    };
+
+    const init = () => {
+        return mongoose.connect(MONGO_CONNECTION_STRING, {useNewUrlParser: true}).then(() => {
+            console.log("Database connection established");
+            return mongo;
+        });
+    };
+
+    return {
+        init,
+    }
 })();
