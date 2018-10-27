28af8cf1031c33c31d15911a98362f67042da126
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-10-27 16:23:10 +0200

    Adds a method to update the database with the email values

---
+++ backend/src/services/ClientUpdateService.ts
@@ -0,0 +1,24 @@
+import { MongoServiceT } from "../types/services/mongo_service";
+import { ImapService } from "../types/services/imap_service";
+
+export const ClientUpdateService = (mongo: MongoServiceT) => {
+
+    const updateDatabase = async () => {
+        ImapService.fetchAttachments(async (update: any[]) => {
+            const updates = [];
+            for (let i = 0; i < update.length; ++i) {
+                updates.push(mongo.updateClientFromEmail(
+                    update[i].Benutzername,
+                    Number.parseInt(update[i]["Jobs gesamt"]),
+                    Number.parseInt(update[i]["Seiten gesamt"])));
+            }
+            for (let i = 0; i < updates.length; ++i) {
+                await updates[i];
+            }
+        });
+    };
+
+    return {
+        updateDatabase,
+    }
+};
