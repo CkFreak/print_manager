7e48cc93dec7bd60544a228d23682b2a5d67fe89
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-10-27 17:11:49 +0200

    Adds a check to see if the object contains all information that is required

--- backend/src/services/ClientUpdateService.ts
+++ backend/src/services/ClientUpdateService.ts
@@ -7,10 +7,12 @@ export const ClientUpdateService = (mongo: MongoServiceT) => {
         ImapService.fetchAttachments(async (update: any[]) => {
             const updates = [];
             for (let i = 0; i < update.length; ++i) {
-                updates.push(mongo.updateClientFromEmail(
-                    update[i].Benutzername,
-                    Number.parseInt(update[i]["Jobs gesamt"]),
-                    Number.parseInt(update[i]["Seiten gesamt"])));
+                if (update[i].Benutzername && update[i]["Jobs gesamt"] && update[i]["Seiten gesamt"]) {
+                    updates.push(mongo.updateClientFromEmail(
+                        update[i].Benutzername,
+                        Number.parseInt(update[i]["Jobs gesamt"]),
+                        Number.parseInt(update[i]["Seiten gesamt"])));
+                }
             }
             for (let i = 0; i < updates.length; ++i) {
                 await updates[i];
