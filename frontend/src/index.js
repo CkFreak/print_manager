666731b5f391e13d75596c885b5e1f68980688fd
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-10-27 22:21:52 +0200

    Changes the standart to a router component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
+import App from './container/App';
 import * as serviceWorker from './serviceWorker';
+import Login from "./container/Login";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route path={"/"} exact component={Login}/>
+            <Route path={"/dashboard"} component={App}/>
+        </div>
+    </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
