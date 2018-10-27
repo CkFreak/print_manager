5c418a27cd37a93910e123450255c410614b65fa
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-10-27 16:23:30 +0200

    Adds some methods to maipulate client data

--- backend/src/services/mongo_service.ts
+++ backend/src/services/mongo_service.ts
@@ -2,6 +2,7 @@ import * as mongoose from "mongoose";
 import { MONGO_CONNECTION_STRING } from "../config/constants";
 import { MongoServiceT } from "../types/services/mongo_service";
 import { UserModel } from "../models/UserModel";
+import { ClientModel } from "../models/ClientModel";
 
 export const MongoService = (() => {
 
@@ -16,9 +17,27 @@ export const MongoService = (() => {
         return UserModel.findOne({name: name}).exec();
     };
 
+    const updateClientFromEmail = (name: string, totalJobs: number, totalPages: number): Promise<any> => {
+        return ClientModel.updateOne({name: name}, {
+            "$inc": {
+                totalJobs: totalJobs,
+                totalPages: totalPages,
+                duePages: totalPages,
+            }
+        }).exec();
+    };
+
+    const markPaymentForClient = (name: string): Promise<any> => {
+        return ClientModel.updateOne({name: name}, {
+            duePages: 0,
+        }).exec();
+    };
+
     const mongo: MongoServiceT = {
         registerUser,
         getUserByName,
+        updateClientFromEmail,
+        markPaymentForClient,
     };
 
     const init = () => {
--- backend/src/types/services/mongo_service.d.ts
+++ backend/src/types/services/mongo_service.d.ts
@@ -1,4 +1,6 @@
 export interface MongoServiceT {
     registerUser(name: string, password: string): Promise<any>;
     getUserByName(name: string): Promise<any>;
+    updateClientFromEmail(name: string, totalJobs: number, totalPages: number): Promise<any>;
+    markPaymentForClient(name: string): Promise<any>;
 }
