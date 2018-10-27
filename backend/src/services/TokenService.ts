19fcb30f81ddae83f04858ab69a8448f4e021aa7
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-10-27 15:23:46 +0200

    Creates a token service to sign the login token

---
+++ backend/src/services/TokenService.ts
@@ -0,0 +1,21 @@
+import * as jsonwebtoken from "jsonwebtoken";
+import * as bluebird from "bluebird";
+import { LOGIN_SECRET } from "../config/constants";
+
+export const TokenService =  () => {
+
+    const jwt: any = bluebird.promisifyAll(jsonwebtoken);
+
+    const signLoginToken = (name: string) => {
+        jwt.signAsync({name}, LOGIN_SECRET, {expiresIn: "1h"}).then((token: string) => {
+            if (token) return token;
+        }).catch((err: Error): any => {
+            console.error(err);
+            return undefined;
+        });
+    };
+
+    return {
+        signLoginToken,
+    }
+};
