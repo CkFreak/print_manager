b03492a73523f2b1e0ac7caf01c5af268996d11d
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-10-27 15:23:14 +0200

    Creates a middleware that checks the token for validity

--- backend/src/middleware/login_status_middleware.ts
+++ backend/src/middleware/login_status_middleware.ts
@@ -1,12 +1,27 @@
 import { Request, Response } from "express";
+import * as jsonwebtoken from "jsonwebtoken";
+import * as bluebird from "bluebird";
+import { LOGIN_SECRET } from "../config/constants";
 
 export const LoginStatusMiddleware = () => {
 
-    const isTokenValid = (token: string) => {
-        
+    const jwt: any = bluebird.promisifyAll(jsonwebtoken);
+
+    const isTokenValid = (req: Request, res: Response, next: Function) => {
+        const token = req.get("jwt");
+        jwt.verifyAsync(token, LOGIN_SECRET).then((decoded: string) => {
+            if (decoded) {
+                console.log(decoded);
+                next();
+            }
+        }).catch((err: Error) => {
+            console.log(err);
+            res.status(401).send({message: "You are not allowed here."});
+            return;
+        });
     };
 
     return {
-
+        isTokenValid,
     };
 };
