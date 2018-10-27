08d2c6d778bcae8f52186277c218fed9041e3502
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-10-27 14:24:14 +0200

    Removes some tests

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,11 @@
 import { MongoService } from "./services/mongo_service";
 import { App } from "./app";
 import { PORT } from "./config/constants";
-import { ImapService } from "./types/services/imap_service";
 
 const start = async () => {
     MongoService.init().then((mongo) => {
         const app = App(mongo);
         app.listen(PORT);
-        ImapService.startService();
     });
 };
 
