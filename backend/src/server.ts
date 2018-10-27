51e880a6c98cb8c9f58f85b9a32a030e0c2e4628
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-10-27 17:11:28 +0200

    Adds a schedule for periodic email checking

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,16 @@
 import { MongoService } from "./services/mongo_service";
 import { App } from "./app";
+import { ClientUpdateService } from "./services/ClientUpdateService";
+import * as schedule from "node-schedule";
 import { PORT } from "./config/constants";
 
 const start = async () => {
+    let clientUpdateService;
     MongoService.init().then((mongo) => {
+        clientUpdateService = ClientUpdateService(mongo);
+        clientUpdateService.updateDatabase();
+        console.info("Scheduling Email Job for 12:02 on every 25th of the year");
+        schedule.scheduleJob("* 2 12 25 * *", clientUpdateService.updateDatabase);
         const app = App(mongo);
         app.listen(PORT);
     });
