37647fec81df9f8649f06c5ed9afd0be5376d4f1
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-10-27 22:44:48 +0200

    Adds a component to be rendered when logged out

--- frontend/src/container/App.js
+++ frontend/src/container/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
+import {Link} from "react-router-dom";
 import '../App.css';
 import { Navbar } from "../components/Navbar";
 
 class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            loggedIn: localStorage.getItem("token") !== null
+        }
+    }
+
   render() {
     return (
         <div className="App">
             <Navbar>
                 Christophorus Print Manager
             </Navbar>
-            <div className={"container-fluid"}>
+            {this.state.loggedIn ?
+            <div>
+                You are logged in
+            </div> :
+                <div>
+                    You are not logged in!<br/>
+                    <Link to={"/"}>Zurück zum Login</Link>
+                </div>
+            }
 
-            </div>
         </div>
     );
   }
