d8d809af44350eb70b4b9254b11531a4486d34b9
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-10-27 14:37:11 +0200

    Installs jsonwebtoken and creates a middleware to check for login status

---
+++ backend/src/middleware/login_status_middleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+
+export const LoginStatusMiddleware = () => {
+
+    const isTokenValid = (token: string) => {
+        
+    };
+
+    return {
+
+    };
+};
