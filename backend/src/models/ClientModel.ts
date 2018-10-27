a7d4bd0f70ceb12d9af2a8484adb48d125212e1a
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-10-27 17:11:12 +0200

    Finishes the client model

--- backend/src/models/ClientModel.ts
+++ backend/src/models/ClientModel.ts
@@ -4,7 +4,7 @@ const ClientSchema = new Schema({
     name: {type: String, unique: true},
     jobsTotal: {type: Number},
     pagesTotal: {type: Number},
-    duePages: {type: Number},
+    duePages: {type: Number, default: 0},
 });
 
 export const ClientModel = model("Client", ClientSchema);
