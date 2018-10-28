139f065047949502584de290864c8a7a8bd91734
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-10-28 14:34:51 +0100

    Adds a logout button to the navvar when logged in

---
+++ frontend/src/components/Navbar/styles/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Logout = styled.a`
+    position: fixed;
+    top: 20px;
+    right: 10px;
+    font-size: 15px;
+    &:hover {
+        cursor: pointer;
+    }
+`;
