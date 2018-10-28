907c04c28dde706f9e079abc22b4452f3ff7a4f6
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-10-28 14:35:57 +0100

    Adds the spinner to login component

--- frontend/src/container/Login.js
+++ frontend/src/container/Login.js
@@ -4,6 +4,10 @@ import "../App.css"
 import { LoginForm } from "../components/Login";
 import axios from "axios";
 import { BASE_URL, LOGIN } from "../config/constants";
+import { Footer } from "../components/Footer";
+import { Spinner } from "../components/Spinner";
+
+// Colors for this project where found here: https://color.adobe.com/Color-Theme-3-color-theme-11432376/edit/?copy=true&base=2&rule=Custom&selected=4&name=Copy%20of%20Color%20Theme%203&mode=rgb&rgbvalues=0.0862745,0.278431,0.45098,0.776471,0.85098,0.611765,0.9098039215686274,0.8627450980392157,0.6862745098039216,0.94902,0.8,0.0588235,0.65098,0.196078,0.196078&swatchOrder=0,1,2,3,4
 
 export default class Login extends Component {
 
@@ -12,11 +16,13 @@ export default class Login extends Component {
         this.state = {
             username: "",
             password: "",
+            loading: false,
         };
     }
 
     login() {
         console.log("Logging in");
+        this.setState({loading: true});
         axios.post(`${BASE_URL}${LOGIN}`, {
             name: this.state.username,
             password: this.state.password
@@ -44,10 +50,15 @@ export default class Login extends Component {
                     Christophorus Print Manager
                 </Navbar>
                 <div className={"container-fluid"}>
-                    <LoginForm login={this.login.bind(this)} onPasswordChange={this.onPasswordChange.bind(this)}
-                               onUserChange={this.onUserChange.bind(this)} password={this.state.password}
-                               username={this.state.username}/>
+                    {this.state.loading ? <Spinner/> :
+                        <LoginForm login={this.login.bind(this)} onPasswordChange={this.onPasswordChange.bind(this)}
+                                   onUserChange={this.onUserChange.bind(this)} password={this.state.password}
+                                   username={this.state.username}/>
+                    }
                 </div>
+                <footer>
+                    <Footer/>
+                </footer>
             </div>
         );
     }
