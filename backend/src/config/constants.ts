1011207ddb87f6bc54fad1bfd7aabfbe4df044ec
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-10-29 11:15:49 +0100

    Installs a module for handling env variables and changes the constants to use those

--- backend/src/config/constants.ts
+++ backend/src/config/constants.ts
@@ -1,5 +1,8 @@
-export const PORT = 3001;
-export const MONGO_CONNECTION_STRING = "mongodb://localhost:27017/print_manager";
+import * as result from "dotenv";
 
+result.config();
+
+export const PORT = process.env.PORT;
+export const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING;
 export const CH_DRUCK_PW = process.env.CH_DRUCK_PW;
 export const LOGIN_SECRET = process.env.LOGIN_SECRET;
