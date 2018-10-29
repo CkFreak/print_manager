9a64ed77dcf36fd5ae07396259a93727b5bf7223
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-10-29 12:57:11 +0100

    Changes where the base url points

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -1,5 +1,5 @@
- export const BASE_URL = "http://localhost:3001";
-//export const BASE_URL = "https://timbo.link/api/print";
+// export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = "https://timbo.link/api/print";
 export const LOGIN = "/login";
 export const PAY = "/pay";
 export const CLIENTS = "/client";
